31760b030eff2f840e6303c63a8bd86dcb005281
ramidhouib <dhouib@rhrk.uni-kl.de>
2020-09-21 21:47:38 +0200

    changes...

--- app/src/main/java/com/myprofile/learn_french/intro.java
+++ app/src/main/java/com/myprofile/learn_french/intro.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,19 +16,18 @@ public class intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        Log.i("info","class intro");
         Intent intent = new Intent(this,intro.class);
         timer = new Timer();
-        MediaPlayer player=MediaPlayer.create(this,R.raw.disguisedpanda);
+        final MediaPlayer player=MediaPlayer.create(this,R.raw.disguisedpanda);
         player.start();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 Intent intent = new Intent(intro.this ,MainActivity.class);
+                player.release();
                 startActivity(intent);
                 finish();
             }
-        }, 5000);
-        player.release();
-        startActivity(intent);
-    }
+        }, 20000); }
 }
